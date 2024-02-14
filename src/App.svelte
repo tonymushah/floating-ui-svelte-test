6c213c6cc63f05890872749c8d22025f11604bd4
tonymushah <tonyramanantsalama@gmail.com>
2024-02-14 16:58:05 +0300

    Added Button and tooltip elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+  let button: HTMLButtonElement;
+  let tooltip: HTMLDivElement
 </script>
 
 <main>
-
+  <button bind:this={button}>
+    My Button
+  </button>
+  <div role="tooltip" bind:this={tooltip}>My tooltip</div>
 </main>
 
 <style lang="scss">
-  
 </style>
