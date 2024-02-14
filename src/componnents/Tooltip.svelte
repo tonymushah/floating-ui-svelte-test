7420e5bc6b6b8349a84fe7d1c643a6fb015958b1
tonymushah <tonyramanantsalama@gmail.com>
2024-02-14 17:34:24 +0300

    Added arrow placement logic

--- src/componnents/Tooltip.svelte
+++ src/componnents/Tooltip.svelte
@@ -1,26 +1,48 @@
 <script lang="ts">
-  import { flip, offset } from "@floating-ui/dom";
+  import { flip, offset, arrow } from "@floating-ui/dom";
   import { shift } from "@floating-ui/dom";
   import { computePosition } from "@floating-ui/dom";
   import "@fontsource/poppins/devanagari.css";
   import { onMount } from "svelte";
   let button: HTMLButtonElement;
   let tooltip: HTMLDivElement;
-  let arrow: HTMLDivElement;
+  let arrowElement: HTMLDivElement;
   onMount(async () => {
-    let { x, y } = await computePosition(button, tooltip, {
-      placement: "top",
-      middleware: [
-        offset(6),
-        flip(),
-        shift({
-          padding: 5,
-        }),
-      ],
-    });
+    const { x, y, placement, middlewareData } = await computePosition(
+      button,
+      tooltip,
+      {
+        placement: "top",
+        middleware: [
+          offset(6),
+          flip(),
+          shift({
+            padding: 5,
+          }),
+          arrow({
+            element: arrowElement,
+          }),
+        ],
+      }
+    );
     Object.assign(tooltip.style, {
       left: `${x}px`,
       top: `${y}px`,
+    });
+    const { x: arrowX, y: arrowY } = middlewareData.arrow;
+    const staticSide = {
+      top: "bottom",
+      right: "left",
+      bottom: "top",
+      left: "right",
+    }[placement.split("-")[0]];
+
+    Object.assign(arrowElement.style, {
+      left: arrowX != null ? `${arrowX}px` : "",
+      top: arrowY != null ? `${arrowY}px` : "",
+      right: "",
+      bottom: "",
+      [staticSide]: "-4px",
     });
   });
 </script>
@@ -31,7 +53,7 @@
   </button>
   <div id="button-tooltip" class:tooltip role="tooltip" bind:this={tooltip}>
     My tooltip with more content
-    <div class="arrow" bind:this={arrow} />
+    <div class="arrow" bind:this={arrowElement} />
   </div>
 </main>
 
